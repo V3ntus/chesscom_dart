d7af9fbdbfef7548aeaee0f921d65d11ee376b19
Ventus <29584664+V3ntus@users.noreply.github.com>
2023-04-30 13:40:45 -0500

    feat: implement http request execution handler

--- lib/internal/http/http_handler.dart
+++ lib/internal/http/http_handler.dart
@@ -1,4 +1,7 @@
 import 'package:chesscom_dart/internal/api.dart';
+import 'package:chesscom_dart/internal/http/http_request.dart';
+import 'package:chesscom_dart/internal/http/http_response.dart';
+
 import 'package:http/http.dart' as http;
 import 'package:logging/logging.dart';
 
@@ -9,7 +12,28 @@ class HttpHandler {
 
   final ChessAPI client;
 
+  HttpHandler(this.client) {
+    httpClient = http.Client();
+  }
 
+  Future<HttpResponse> execute(HttpRequest request) async {
+    logger.fine('Handling request execution $request');
+    logger.finer([...request.headers.entries.map((e) => "${e.key}: ${e.value}")]
+        .join("\n"));
 
-  HttpHandler(this.client);
+    final response = await HttpResponse.fromResponse(
+        await httpClient.send(request.prepare()));
+
+    if (response.code >= 200 && response.code < 300) {
+      logger.finest("Successful response: $response");
+      return response;
+    } else if (response.code == 429) {
+      logger.warning("429 Rate limited. Retrying...");
+      return Future.delayed(Duration(seconds: 1), () => execute(request));
+    }
+    logger.finest("Unknown response: ${response.toString()}");
+    return response;
+  }
+
+  Future<void> dispose() async => httpClient.close();
 }
