e0c56512ff794a0a3a3924286d00bece8ea37f10
Ventus <29584664+V3ntus@users.noreply.github.com>
2023-04-30 13:40:27 -0500

    feat: implement request prepare for HttpRequest

--- lib/internal/http/http_request.dart
+++ lib/internal/http/http_request.dart
@@ -1,3 +1,5 @@
+import 'package:http/http.dart' as http;
+
 import 'package:chesscom_dart/internal/constants.dart';
 import 'package:chesscom_dart/internal/http/http_endpoints.dart';
 
@@ -7,10 +9,18 @@ class HttpRequest {
 
   final String method;
   final Map<String, dynamic>? queryParams;
-  
+
   final HttpEndpoint endpoint;
-  
-  HttpRequest(this.endpoint, {this.method = "GET", this.queryParams, Map<String, String>? headers}) {
+
+  http.BaseRequest prepare() => http.Request(
+      method,
+      uri.replace(
+          queryParameters: queryParams
+              ?.map((key, value) => MapEntry(key, value.toString()))))
+    ..headers.addAll(headers);
+
+  HttpRequest(this.endpoint,
+      {this.method = "GET", this.queryParams, Map<String, String>? headers}) {
     uri = Uri.https(Constants.host, Constants.baseUri + endpoint.path);
     this.headers = headers ?? {};
     this.headers.addAll({
