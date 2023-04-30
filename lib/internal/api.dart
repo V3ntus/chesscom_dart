4c7cf6183313d53ab9701573932c592c2b8052fe
Ventus <29584664+V3ntus@users.noreply.github.com>
2023-04-30 13:41:56 -0500

    feat: client factory logic
    feat: fetchProfile

--- lib/internal/api.dart
+++ lib/internal/api.dart
@@ -1,5 +1,29 @@
+import 'package:chesscom_dart/internal/http/http_endpoints.dart';
+import 'package:chesscom_dart/internal/http/http_handler.dart';
+import 'package:chesscom_dart/internal/http/http_request.dart';
+
+import 'package:logging/logging.dart';
+
 abstract class ChessFactory {
-  static ChessAPI createClient() => ChessAPI();
+  /// Create an HTTP client instance that connects to the Chess.com API
+  static ChessAPI createClient({Level logLevel = Level.INFO}) => ChessAPI(logLevel: logLevel);
 }
 
-class ChessAPI {}
+class ChessAPI {
+  late final HttpHandler httpHandler;
+  final Level logLevel;
+
+  Future<Map<String, dynamic>?> fetchProfile(String username) async {
+    final res = await httpHandler.execute(HttpRequest(HttpEndpoint()..player(username)));
+    return res.json;
+  }
+
+  ChessAPI({this.logLevel = Level.INFO}) {
+    Logger.root.level = logLevel;
+    Logger.root.onRecord.listen((record) {
+      print('[${record.level.name}] ${record.time} - ${record.message}');
+    });
+
+    httpHandler = HttpHandler(this);
+  }
+}
