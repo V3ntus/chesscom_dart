4658b046f004cb373f774635e52c546e46937f04
Ventus <29584664+V3ntus@users.noreply.github.com>
2023-04-30 19:13:16 -0500

    ref: correct player model path

--- lib/chesscom_dart.dart
+++ lib/chesscom_dart.dart
@@ -1,3 +1,3 @@
 library chesscom_dart;
 export 'package:chesscom_dart/internal/api.dart' show ChessAPI, ChessFactory;
-export 'package:chesscom_dart/internal/models/player.dart' show IPlayer;
+export 'package:chesscom_dart/internal/models/player/player.dart' show IPlayer;
